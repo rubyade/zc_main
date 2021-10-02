5955b1a225a6cd0b1d074bb4d1c26afc651c137f
Ruby Adeniregun <rubyadeniregun@gmail.com>
2021-10-02 20:04:38 +0100

    resolved task that allows user with one workspace to login directly

--- control/src/pages/login/index.js
+++ control/src/pages/login/index.js
@@ -6,10 +6,9 @@ import FormWrapper from '../../components/AuthFormWrapper'
 import LoginLoading from '../../components/LoginLoading'
 import styles from '../../component-styles/AuthFormElements.module.css'
 import axios from 'axios'
-import { GetUserInfo } from '@zuri/control'
+import { GetUserInfo } from '../../zuri-control'
 import $behaviorSubject from '../../../../globalState'
 import { Helmet } from 'react-helmet'
-import { goToDefaultChannel } from '../../api/channels'
 // import { Link } from 'react-router-dom'
 // import authBg1 from './assets/auth_bg1.svg'
 // import authBg2 from './assets/auth_bg2.svg'
@@ -17,7 +16,6 @@ import { goToDefaultChannel } from '../../api/channels'
 // import authBg4 from './assets/auth_bg4.svg'
 // import authBg5 from './assets/auth_bg5.svg'
 //import GoogleLogin from 'react-google-login'
-
 const Login = () => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
@@ -26,47 +24,39 @@ const Login = () => {
   const [passworderror, setpassworderror] = useState('')
   const [rememberMe, setRememberMe] = useState('')
   const [Loading, setLoading] = useState(false)
-
   // Background Images
   // const images = [authBg1, authBg2, authBg3, authBg4, authBg5]
   // const [currentImage, setcurrentImage] = useState(
   //   Math.floor(Math.random() * 4)
   // )
-
   // To Display Random Aside Background Image
   // const displayImage = () => {
   //   let i = currentImage
   //   i >= images.length - 1 ? (i = 0) : i++
   //   setcurrentImage(i)
   // }
-
   let history = useHistory()
-
   // const redirect = location.search ? location.search.split("=")[1] : "/";
-
   useEffect(() => {
     const userInfo = sessionStorage.getItem(`user`)
     const redirect = sessionStorage.getItem(`workSpaceInviteRedirect`)
-
-    if (userInfo && userInfo !== null && redirect !== null)
+    if (userInfo && userInfo !== null && redirect !== null) {
       history.push(redirect)
+    } else {
+    }
   }, [history])
-
   const handleSubmit = async e => {
     e.preventDefault()
     setemailerror('')
     setpassworderror('')
-
     if (!email) {
       setemailerror(`Enter an email address`)
       return
     }
-
     if (!password) {
       setpassworderror(`Enter a Password`)
       return
     }
-
     await axios
       .post('https://api.zuri.chat/auth/login', {
         email,
@@ -74,65 +64,27 @@ const Login = () => {
       })
       .then(response => {
         const { data, message } = response.data
-
-        setLoading(true)
-
         //Store token in localstorage
         sessionStorage.setItem('token', data.user.token)
-
         //Store token in localstorage
         sessionStorage.setItem('session_id', data.session_id)
-
         //Store user copy in localstorage
         sessionStorage.setItem('user', JSON.stringify(data.user))
-
-        //Return the login data globally
-        $behaviorSubject.next(response.data)
-
-        // Switch for redirects
-        axios
-          .get(`https://api.zuri.chat/users/${data.user.id}`, {
-            headers: {
-              Authorization: `Bearer ${data.user.token}`
-            }
-          })
-          .then(res => {
-            const orgs = res.data.data['Organizations'].length
-            console.log('reg orgs', orgs)
-            localStorage.setItem(
-              'currentWorkspace',
-              res.data.data['Organizations'][0]
-            )
-
-            switch (true) {
-              case orgs > 1:
-                history.push('/choose-workspace')
-                break
-              case orgs < 1:
-                history.push('/createworkspace')
-                break
-              default:
-                goToDefaultChannel()
-            }
-          })
-          .catch(err => {
-            throw err
-          })
-
         //Display message
         // alert(message) //Change this when there is a design
-
-        // setTimeout(() => {
-        //Redirect to some other page
-        //   GetUserInfo()
-        //   history.push('/choose-workspace')
-        //   setLoading(false)
-        // }, 2000)
+        //Return the login data globally
+        $behaviorSubject.next(response.data)
+        setLoading(true)
+        getOrganizations(data.user.email, data.user.token)
+        //setTimeout(() => {
+        //  //Redirect to some other page
+        //  history.push('/choose-workspace')
+        //  setLoading(false)
+        //}, 2000)
       })
       .catch(error => {
         const { data } = error.response
         console.log(data)
-
         RegExp('not found').test(data.message) &&
           setemailerror(
             'Sorry, this email is not registered, try again or click Create an Account.'
@@ -141,12 +93,42 @@ const Login = () => {
           setpassworderror(
             'Sorry, you have entered the wrong password. Try again or click Get help signing in.'
           )
-
         //Render error message to the user
         seterror(data.message) //Change this when there is a design
       })
   }
-
+  const getOrganizations = async (userEmail, token) => {
+    try {
+      const response = await axios.get(
+        `https://api.zuri.chat/users/${userEmail}/organizations`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`
+          }
+        }
+      )
+      if (response.status !== 200) {
+        throw new Error(
+          `Unable to fetch list of wokspaces, status code: ${response.status}`
+        )
+      }
+      const { data } = await response.data
+      console.log(data)
+      if (!data.length) {
+        setLoading(false)
+        history.push('/signup')
+      } else if (data.length === 1) {
+        setLoading(false)
+        history.push('/home')
+      } else if (data.length > 1) {
+        setLoading(false)
+        history.push('/choose-workspace')
+      }
+    } catch (error) {
+      setLoading(false)
+      console.log(error)
+    }
+  }
   return (
     <main id={styles.authPageWrapper}>
       <Helmet>
@@ -195,7 +177,6 @@ const Login = () => {
             error={passworderror}
             // onFocus={displayImage}
           />
-
           <div className={`${styles.rememberMe}`}>
             <div className={`${styles.left}`}>
               <input
@@ -219,5 +200,4 @@ const Login = () => {
     </main>
   )
 }
-
 export default withRouter(Login)
